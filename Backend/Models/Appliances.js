ce7ab332f983166c3a4b80f8b454e6e69d916f3a
ALIGCJH <simonjuma465@gmail.com>
2025-03-27 14:18:56 +0300

    updated Boresha-power

---
+++ Backend/Models/Appliances.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const applianceSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  expectedWatts: { type: Number, required: true },
+  currentWatts: { type: Number, required: true },
+  userPhone: { type: String, required: true }
+});
+
+const Appliance = mongoose.model("Appliance", applianceSchema);
+
+module.exports = Appliance;
