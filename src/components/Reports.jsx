00ed9e4c986012b82cd568ae710ef844e4673fb0
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-03-28 12:29:01 +0300

    fix: resolve merge conflict

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -44,23 +44,6 @@ export const Reports = () => {
   return (
     <AnimatedPage>
 
-     <div className="report">
-      <div clasName="header">
-      <h1 className="heading">BoreshaPower</h1>
-      <h2 className="heading2">Track you Power Track your Future</h2>
-      </div>
-      <div className="report-page">
-      <h3>Weekly Energy Usage Report</h3>
-<div className="chart">
-
-<div >
-</div>
-
-</div>
-      </div>
-      
-     </div>
-=======
       <div className="reports-container">
         <h1>Power Up Your Savings: ENERGY USAGE</h1>
         <div className="top">
